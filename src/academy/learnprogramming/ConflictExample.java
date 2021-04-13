19bd95bec84224ec63e8591ba959cace973f50c6
Anastasia Hristian <an.hristian@gmail.com>
2021-04-13 13:47:36 -0600

    conflicts with imports

---
+++ src/academy/learnprogramming/ConflictExample.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+import java.util.*;
+import java.sql.*;
+
+public class ConflictExample {
+
+    public static void main(String[] args) {
+        java.util.Date date;
+        java.sql.Date sqlDate;
+
+    }
+}
